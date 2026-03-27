a113d02ed9ac115e6f754cea93cf76ff5824edb5
urkey-ops <keyurdudakiya@gmail.com>
2026-03-27 15:03:58 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -7,4 +7,4 @@ const APP_VERSION = 'v6';
 // To generate: in browser console run:
 //   crypto.subtle.digest('SHA-256', new TextEncoder().encode('yourpin'))
 //     .then(b => console.log(Array.from(new Uint8Array(b)).map(x=>x.toString(16).padStart(2,'0')).join('')))
-const HARDCODED_PIN_HASH = '8bb0cf6eb9b17d0f7d22b456f121257dc1254e1f01665370476383ea776df414';
+const HARDCODED_PIN_HASH = '2a8610aefdd0028c6bf074dd18721c0ef8bc43241cc7a653d7aedf2036bdf6b3';
